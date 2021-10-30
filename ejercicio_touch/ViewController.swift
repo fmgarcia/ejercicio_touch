3bb97188d5e16490a6de952f048876d2b1062c82
Fran García <frangarcia2707@gmail.com>
2021-10-30 16:32:33 -0700

    Ejercicio gestos touch acabado

--- ejercicio_touch/ViewController.swift
+++ ejercicio_touch/ViewController.swift
@@ -9,10 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var tocaImagen = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.imageView.isMultipleTouchEnabled = true
+
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {  // Sólo nos centramos en el primer toque
+            print("Touches began")
+
+            let touchCount = event!.allTouches!.count // Número de dedos pulsando
+            let tapCount = touch.tapCount  // Núero de pulsaciones rápidas
+
+            print("Finger count= \(touchCount)")
+            print("Tap count= \(tapCount)")
+
+            let loc = touch.location(in: self.view)
+            if self.imageView.frame.contains(loc) {
+                print("Detectado toque sobre la imagen")
+                self.tocaImagen = true
+            }
+        }
+        super.touchesBegan(touches, with: event)
+    }
+
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            print("Touches moved")
+            let loc = touch.location(in: self.view)
+            if self.tocaImagen == true {
+                self.imageView.center = loc
+            }
+        }
+        super.touchesMoved(touches, with: event)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+            print("Touches ended")
+            self.tocaImagen = false
+            super.touchesEnded(touches, with: event)
+        
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+            print("Touches cancelled")
+            self.tocaImagen = false
+            super.touchesCancelled(touches, with: event)
+    }
+
+    
 
 
 }
